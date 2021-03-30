ee0fd48f48353967e09206624e798ec89e6c7134
“Paul <2139206e@student.gla.ac.uk>
2021-03-30 01:29:15 +0100

    added some jsquery, will add more when i've found out exact buttons, colours etc we want

--- static/js/RateMyWalk-jquery.js
+++ static/js/RateMyWalk-jquery.js
@@ -4,5 +4,16 @@ $(document).ready(function() {
     $('#unauthenticated-user-rating').click(function() {
         alert('Please Log In to rate this walk');
     });
+    
+    $('.btn btn-primary').hover(
+    	function() {
+    		$(this).css('background-color', 'grey');
+    		$(this).css('color', 'black');
+    	},
+    	function() {
+    		$(this).css('background-color', 'white');
+    	},
+	});
 
 })
+
