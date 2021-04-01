1f7ea436446ed03e236e8e0ef70d06143dd86449
“Paul <2139206e@student.gla.ac.uk>
2021-04-01 18:27:06 +0100

    altered myprofile.html, add password button is now a proper button - link still not working

--- static/js/RateMyWalk-jquery.js
+++ static/js/RateMyWalk-jquery.js
@@ -23,7 +23,23 @@ $(document).ready(function() {
 		},
 	});
 	
+    $("#top-5-walks").hover(
+		function() {
+            $('ul.file_menu').stop(true, true).slideDown('medium');
+        },
+        function() {
+            $('ul.file_menu').stop(true, true).slideUp('medium');
+        }
 	
+	$('.py-1 mb-2 bg-dark rounded btn').hover(
+		function() {
+			$(this).css('background-color', 'grey');
+		},
+    	function() {
+			$(this).css('background-color', 'black');
+		},
+	});
+	/*
 	$('.rango-page-add').click(function() {
 		var categoryid = $(this).attr('data-categoryid');
 		var url = $(this).attr('data-url');
@@ -36,4 +52,5 @@ $(document).ready(function() {
     			clickedButton.hide();
 			})
 	})
+	*/
 });
