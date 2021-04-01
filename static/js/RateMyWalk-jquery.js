6c934d4508987e8245d686f039dcdb58b67cd32c
Ryan Gregson <2469038G@student.gla.ac.uk>
2021-04-01 21:12:49 +0100

    Rate Walk Yourself button removed. JQuery added to get background colour of nav bar.

--- static/js/RateMyWalk-jquery.js
+++ static/js/RateMyWalk-jquery.js
@@ -4,53 +4,32 @@ $(document).ready(function() {
     $('#unauthenticated-user-rating').click(function() {
         alert('Please Log In to rate this walk');
     });
-    
-    $('.btn btn-primary').hover(
+
+    $('.navbarText').hover(
     	function() {
-    		$(this).css('background-color', 'grey');
+    		$(this).css('color', 'grey');
     	},
     	function() {
+    		$(this).css('color', 'white');
+    });
+    
+    var bgOn;
+    $('a').hover(
+    	function() {
+            bgOn = $(this).css('background-color');
     		$(this).css('background-color', 'black');
     	},
-	});
-	
-	$('.navbarText').hover(
-		function() {
-			$(this).css('background-color', 'grey');
-		},
     	function() {
-			$(this).css('background-color', 'black');
-		},
-	});
-	
+    		$(this).css('background-color', bgOn);
+    });
+    
+    
     $("#top-5-walks").hover(
 		function() {
             $('ul.file_menu').stop(true, true).slideDown('medium');
         },
         function() {
             $('ul.file_menu').stop(true, true).slideUp('medium');
-        }
-	
-	$('.py-1 mb-2 bg-dark rounded btn').hover(
-		function() {
-			$(this).css('background-color', 'grey');
-		},
-    	function() {
-			$(this).css('background-color', 'black');
-		},
-	});
-	/*
-	$('.rango-page-add').click(function() {
-		var categoryid = $(this).attr('data-categoryid');
-		var url = $(this).attr('data-url');
-		var clickedButton = $(this);
-		
-		$.get('/RateMyWalk/rateWalk/',
-			{'ratewalk_id': ratewalk, 'link': url},
-			function(data) {
-				$('#rateWalkYourself').html(data);
-    			clickedButton.hide();
-			})
-	})
-	*/
+    });
+        
 });
