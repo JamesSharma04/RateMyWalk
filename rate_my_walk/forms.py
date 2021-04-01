3b7a9a8d69cd00b5f2b46aaa8647195813dbe47b
“Paul <2139206e@student.gla.ac.uk>
2021-04-01 17:33:57 +0100

    added jquery for hovering over buttons and have started adding button click functionality, however links still not working

---
+++ rate_my_walk/forms.py
@@ -0,0 +1,83 @@
+from django import forms
+from rate_my_walk.models import WalkPage, Comment, Photo, Rating
+from django.utils import timezone
+#for userprofile
+from rate_my_walk.models import UserProfile
+
+class WalkPageForm(forms.ModelForm):
+    
+    name = forms.CharField(max_length=128,
+                           help_text = "Please enter the name of your walk")
+    desc = forms.CharField(max_length=2048,
+                           help_text = "Please write the description of your walk here")
+    start = forms.CharField(max_length=128,
+                            help_text = "Enter start location of your walk")
+    end = forms.CharField(max_length=128,
+                          help_text = "Enter end location of your walk")
+    
+    cover = forms.ImageField(help_text = "Upload picture of your walk", required=False)
+    
+    enjoyment = forms.IntegerField(initial = 0,
+                                   help_text = "Rate enjoyment (0-10)",
+                                   max_value = 10,
+                                   min_value = 0)
+    duration = forms.IntegerField(initial = 0,
+                                  help_text = "Rate duration (0-10)",
+                                  max_value = 10,
+                                  min_value = 0)
+    difficulty = forms.IntegerField(initial = 0, help_text = "Rate difficulty (0-10)",
+                                    max_value = 10,
+                                    min_value = 0)
+    
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+    
+    date = forms.DateField(widget=forms.HiddenInput, required=False)
+    
+    class Meta:
+        model = WalkPage
+        fields = ('name', 'desc', 'start', 'end', 'cover', 'enjoyment', 'duration', 'difficulty', )
+
+
+class RatingForm(forms.ModelForm):
+    duration = forms.IntegerField(help_text="Duration", initial = 0, max_value = 10, min_value = 0)
+    difficulty = forms.IntegerField(help_text="Difficulty", initial = 0, max_value = 10, min_value = 0)
+    enjoyment = forms.IntegerField(help_text="Enjoyment", initial = 0, max_value = 10, min_value = 0)
+
+    class Meta:
+        model = Rating
+        exclude = ('walk', 'rater')
+
+class PhotoForm(forms.ModelForm):
+    	
+	picture = forms.ImageField(help_text="If you also had a walk at this location you can upload a picture here")
+
+	class Meta:
+		model = Photo
+		exclude = ('walk', 'owner', 'date')
+
+class CommentForm(forms.ModelForm):
+    title = forms.CharField(max_length=128,
+                            help_text = "Please enter the title of your comment")
+    comment = forms.CharField(max_length=128,
+                              help_text = "Write your comment here")
+    date = forms.DateField(widget=forms.HiddenInput(), required=False)
+    
+    class Meta:
+        model = Comment
+        exclude = ('owner', 'walk', )
+
+class DeleteWalkForm(forms.ModelForm):
+    slug = forms.SlugField(widget=forms.HiddenInput())
+    
+    class Meta:
+        model = WalkPage
+        fields = ('slug', )
+<<<<<<< HEAD
+        
+=======
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields=('website','picture',)
+>>>>>>> d365b49077cdc25f3111fa2a7cd4a355d1e34fdd
