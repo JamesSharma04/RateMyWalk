0d0ee1789e5db9dd895426272f4be16a71ed953a
Ryan Gregson <2469038G@student.gla.ac.uk>
2021-04-05 19:07:40 +0100

    Removed unnecessary files and migrations folder.

--- rate_my_walk/models.py
+++ rate_my_walk/models.py
@@ -72,7 +72,6 @@ class Photo(models.Model):
 	date = models.DateField(default=timezone.now)
 	owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='Uploader', null=True)
 	picture = models.ImageField(upload_to='more_page_image', default='default.jpg')
-	likes = models.IntegerField(default=0)
 
 class UserProfile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
